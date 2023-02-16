c3aeb068bde7876d07a7ad9771c41ab1cb0824cb
Santiago Pereyra <santi@MSI.localdomain>
2023-02-15 22:43:36 -0300

    Jetstream install and Admin template ready

--- config/app.php
+++ config/app.php
@@ -186,6 +186,8 @@ return [
          * Package Service Providers...
          */
 
+        Spatie\Permission\PermissionServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -194,6 +196,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
 
     ],
 
