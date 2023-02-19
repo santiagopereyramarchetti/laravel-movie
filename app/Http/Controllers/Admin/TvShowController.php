5a84bfb046841b86b55421193b34def3e9f00b46
Santiago Pereyra <santi@MSI.localdomain>
2023-02-19 15:17:47 -0300

    TvShow ready

--- app/Http/Controllers/Admin/TvShowController.php
+++ app/Http/Controllers/Admin/TvShowController.php
@@ -3,12 +3,70 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\TvShow;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class TvShowController extends Controller
 {
-    public function index(){
-        return Inertia::render('TvShows/Index');
+    public function index(Request $request){
+        $perPage = $request->perPage ?? 5;
+        $filters = $request->only('search', 'perPage');
+
+        $tvShows = TvShow::when($request->search, function($tvShowList, $search) {
+            $tvShowList->where('name', 'like', "%{$search}%");
+        })->paginate($perPage)
+        ->withQueryString();
+        
+        return Inertia::render('TvShows/Index', compact('tvShows', 'filters'));
+    }
+
+    public function store(Request $request){
+
+        $tvShow = TvShow::where('tmdb_id', $request->tvShowTMDBId)->first();
+        if($tvShow){
+            return back()->with('flash.banner', 'Already exists');
+        }else{
+            $tmdb_TvShow = Http::get( config('services.tmdb.endpoint').'tv/'. $request->tvShowTMDBId .'?api_key='.config('services.tmdb.secret').'&language=en-US');
+            
+            if ($tmdb_TvShow->successful()) {
+                TvShow::create([
+                    'tmdb_id' => $tmdb_TvShow['id'],
+                    'name'    => $tmdb_TvShow['name'],
+                    'poster_path' => $tmdb_TvShow['poster_path'],
+                    'created_year' => $tmdb_TvShow['first_air_date'],
+                ]);
+                return back()->with('flash.banner', 'TvShow created');
+            }else{
+                return back()->with('flash.banner', 'API error');
+            }
+        }
+
+    }
+
+    public function edit(TvShow $tvShow){
+
+        return Inertia::render('TvShows/Edit', compact('tvShow'));
+
+    }
+
+    public function update(Request $request, TvShow $tvShow){
+        $validated = $request->validate([
+            'name' => ['required'],
+        ]);
+
+        $tvShow->update($validated);
+
+        return to_route('admin.tv-shows.index')->with('flash.banner', 'Tv Show updated');
+    }
+
+    public function destroy(TvShow $tvShow){
+
+        $tvShow->delete();
+
+        return to_route('admin.tv-shows.index')->with('flash.banner', 'Tv Show deleted');
+
     }
 }
--- app/Models/TvShow.php
+++ app/Models/TvShow.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class TvShow extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['tmdb_id', 'name', 'slug', 'poster_path', 'created_year'];
+
+    //We define a model mutator
+    protected function name(): Attribute{
+
+        return Attribute::make(
+            set: fn($value) => [
+                'name' => $value,
+                'slug' => Str::slug($value)
+            ],
+        );
+
+    }
 }
