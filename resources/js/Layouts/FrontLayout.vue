7d02f7bf0d30c15e60030e4055a11ce8d352ca05
Santiago Pereyra <santi@MSI.localdomain>
2023-02-21 12:39:38 -0300

    Front Layout ready

---
+++ resources/js/Layouts/FrontLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import { Head, Link, router } from '@inertiajs/vue3';
+import NavbarFront from '../Components/NavbarFront.vue';
+import Footer from '../Components/Footer.vue';
+
+defineProps({
+    title: String,
+});
+
+</script>
+
+<template>
+    <div>
+        <Head :title="title" />
+
+        <div class="min-h-screen bg-gray-100">
+            <!-- Nav -->
+            <NavbarFront></NavbarFront>
+
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+        </div>
+        <Footer></Footer>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome');
 });
 
 // middleware()->['auth:sanctum', 'verified', 'role:admin']
